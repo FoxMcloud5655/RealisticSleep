5642aa4f067a6dc62d9317273c869cdb3078f9b4
FoxMcloud5655 <support@foxmcloud.net>
2021-02-19 03:33:51 -0600

    Added World Tick Method

--- src/main/java/net/foxmcloud/realisticsleep/RealisticSleep.java
+++ src/main/java/net/foxmcloud/realisticsleep/RealisticSleep.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.server.ChunkHolder;
@@ -35,10 +36,14 @@ public class RealisticSleep
 	public static final Logger LOGGER = LogManager.getLogger("RealisticSleep");
 	public static final String MODNAME = "Realistic Sleep";
 	public static final String MODID = "realisticsleep";
+	public static final String MODID_PROPER = "RealisticSleep";
 	public static final String VERSION = "${mod_version}";
 	public static final boolean DEBUG = true;
 	public static final Method getLoadedChunksMethod;
+	public static final Method tickBlockEntitiesMethod;
 	private final RealisticSleepConfig config = new RealisticSleepConfig();
+	
+	public long lastSleepTicks = 0;
 
 	public RealisticSleep() {
 		ModLoadingContext.get().registerConfig(Type.COMMON, this.config.getSpec());
@@ -48,71 +53,137 @@ public class RealisticSleep
 
 	public void onSetup(FMLCommonSetupEvent event) {
 		if (getLoadedChunksMethod == null) {
-			LOGGER.error("[RealisticSleep] Chunk reflection unsuccessful.  Mod is now useless.");
+			logError("Chunk reflection unsuccessful.  Mod will not work.");
 			return;
 		}
-		LOGGER.info("[RealisticSleep] Chunk reflection successful.");
-		getLoadedChunksMethod.setAccessible(true);
+		else {
+			logInfo("Chunk reflection successful.");
+			getLoadedChunksMethod.setAccessible(true);
+		}
+		if (tickBlockEntitiesMethod == null) {
+			logError("World reflection unsuccessful.  Mod will not work.");
+			return;
+		}
+		else {
+			logInfo("World reflection successful.");
+			tickBlockEntitiesMethod.setAccessible(true);
+		}
 	}
 
 	@SubscribeEvent
 	public void onPlayerSleep(SleepingLocationCheckEvent event) {
-		if (event.getEntityLiving().world.isRemote || getLoadedChunksMethod == null) {
+		if (event.getEntityLiving().world.isRemote) {
 			return;
 		}
 		if (event.getEntityLiving() instanceof ServerPlayerEntity) {
 			ServerPlayerEntity player = (ServerPlayerEntity)event.getEntityLiving();
 			World world = player.getEntityWorld();
-			if (player.isPlayerFullyAsleep() && !world.isRemote) {
-				long ticksUntilMorning = 24000 - world.getDayTime();
-				if (DEBUG) LOGGER.info("[RealisticSleep] Calculated " + ticksUntilMorning + " ticks until morning.");
-				int ticksToSimulate = (int)(ticksUntilMorning / 4);
-				ArrayList<ITickableTileEntity> tileEntities = new ArrayList<ITickableTileEntity>();
-				ServerChunkProvider chunkProvider = (ServerChunkProvider)world.getChunkProvider();
-				try {
-					Iterable<ChunkHolder> chunks = (Iterable<ChunkHolder>)getLoadedChunksMethod.invoke(chunkProvider.chunkManager);
-					chunks.forEach(chunkHolder -> {
-						Chunk chunk = chunkHolder.getChunkIfComplete();
-						if (chunk == null) return;
-						chunk.getTileEntitiesPos().forEach(tileEntityPos -> {
-							TileEntity tileEntity = chunk.getTileEntity(tileEntityPos);
-							if (tileEntity instanceof ITickableTileEntity && config.tileEntityNotInBlacklist(tileEntity)) {
-								if (DEBUG) LOGGER.info("[RealisticSleep] Found TileEntity " + tileEntity.getType().getRegistryName() + " to update.");
-								tileEntities.add((ITickableTileEntity)tileEntity);
-							}
-						});
-					});
-				} catch (Exception e) {
-					LOGGER.error("[RealisticSleep] Error when adding TileEntity to list: " + e.getMessage());
+			if (player.isPlayerFullyAsleep()) {
+				if (!config.canSleep(world.getGameTime() - lastSleepTicks)) {
+					event.setResult(Result.DENY);
+					player.sendStatusMessage(new StringTextComponent("You're not that tired after recently sleeping."), true);
 					return;
 				}
-				if (DEBUG) LOGGER.info("[RealisticSleep] Finished gathering TileEntities.  Simulating " + ticksToSimulate + " ticks...");
-				tileEntities.forEach(tileEntity -> {
-					for (int i = 0; i < ticksToSimulate; i++) {
-						try {
-							tileEntity.tick();
-						} catch (Exception e) {
-							LOGGER.error("[RealisticSleep] Error on TickEvent Handler: " + e.getMessage());
-							return;
-						}
-					}	
-				});
-				if (DEBUG) LOGGER.debug("[RealisticSleep] Tick simulation complete.");
+				long ticksUntilMorning = 24000 - world.getDayTime();
+				if (DEBUG) logInfo("Calculated " + ticksUntilMorning + " ticks until morning.");
+				int ticksToSimulate = (int)(ticksUntilMorning / 4);
+				switch(config.getSimulationMethod()) {
+				case 1:
+					tickTileEntities(ticksToSimulate, world);
+					break;
+				case 2:
+					tickWorld(ticksToSimulate, world);
+					break;
+				default:
+					logError("Invalid configuration.  Please check the config file and use a defined method.");
+					break;
+				}
+				lastSleepTicks = world.getGameTime();
+				if (DEBUG) logInfo("Tick simulation complete.");
 				event.setResult(Result.DEFAULT);
 			}
 		}
+	}
+
+	private void tickTileEntities(int ticksToSimulate, World world) {
+		if (getLoadedChunksMethod != null) {
+			ArrayList<ITickableTileEntity> tileEntities = new ArrayList<ITickableTileEntity>();
+			ServerChunkProvider chunkProvider = (ServerChunkProvider)world.getChunkProvider();
+			try {
+				Iterable<ChunkHolder> chunks = (Iterable<ChunkHolder>)getLoadedChunksMethod.invoke(chunkProvider.chunkManager);
+				chunks.forEach(chunkHolder -> {
+					Chunk chunk = chunkHolder.getChunkIfComplete();
+					if (chunk == null) return;
+					chunk.getTileEntitiesPos().forEach(tileEntityPos -> {
+						TileEntity tileEntity = chunk.getTileEntity(tileEntityPos);
+						if (tileEntity instanceof ITickableTileEntity && config.tileEntityNotInBlacklist(tileEntity)) {
+							if (DEBUG) logInfo("Found TileEntity " + tileEntity.getType().getRegistryName() + " to update.");
+							tileEntities.add((ITickableTileEntity)tileEntity);
+						}
+					});
+				});
+			} catch (Exception e) {
+				logError("Error when adding TileEntity to list: " + e.getMessage());
+				return;
+			}
+			if (DEBUG) logInfo("Finished gathering TileEntities.  Simulating " + ticksToSimulate + " ticks...");
+			tileEntities.forEach(tileEntity -> {
+				for (int i = 0; i < ticksToSimulate; i++) {
+					try {
+						tileEntity.tick();
+					}
+					catch (Exception e) {
+						logError("Error on TickEvent Handler: " + e.getMessage());
+						return;
+					}
+				}	
+			});
+		}
+		else logError("Chunk reflection not valid with current setup.");
+	}
+	
+	private void tickWorld(int ticksToSimulate, World world) {
+		if (tickBlockEntitiesMethod != null) {
+			try {
+				for (int i = 0; i < ticksToSimulate; i++) {
+					tickBlockEntitiesMethod.invoke(world);
+				}
+			}
+			catch (Exception e) {
+				logError("Error when calling tickBlockEntities: " + e.getMessage());
+			}
+		}
+		else logError("Wrold reflection not valid with current setup.");
+	}
+	
+	private static void logInfo(String message) {
+		LOGGER.info("[" + RealisticSleep.MODID_PROPER + "] " + message);
+	}
+	
+	private static void logError(String message) {
+		LOGGER.error("[" + RealisticSleep.MODID_PROPER + "] " + message);
 	}
 
 	private static Method fetchGetLoadedChunksMethod() {
 		try {
 			return ObfuscationReflectionHelper.findMethod(ChunkManager.class, "func_223491_f");
 		} catch (Exception e) {
-			LOGGER.error("[RealisticSleep] Exception occurred while accessing getLoadedChunksIterable: " + e.getMessage());
+			logError("Exception occurred while accessing getLoadedChunksIterable: " + e.getMessage());
+			return null;
+		}
+	}
+	
+	private static Method fetchTickBlockEntitiesMethod() {
+		try {
+			return ObfuscationReflectionHelper.findMethod(World.class, "func_217391_K");
+		} catch (Exception e) {
+			logError("Exception occurred while accessing tickBlockEntities: " + e.getMessage());
 			return null;
 		}
 	}
 
 	static {
 		getLoadedChunksMethod = fetchGetLoadedChunksMethod();
+		tickBlockEntitiesMethod = fetchTickBlockEntitiesMethod();
 	}
 }
--- src/main/java/net/foxmcloud/realisticsleep/RealisticSleepConfig.java
+++ src/main/java/net/foxmcloud/realisticsleep/RealisticSleepConfig.java
@@ -7,20 +7,39 @@ import com.google.common.base.Splitter;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import net.minecraftforge.common.ForgeConfigSpec.LongValue;
 
 public class RealisticSleepConfig {
 
 	private ForgeConfigSpec spec;
+	private IntValue method;
+	private LongValue waitTime;
 	private ConfigValue<String> blacklist;
 
 	public RealisticSleepConfig() {
 		final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+		builder.comment("Defines what method of tick simulation to perform.  The default is 1.\n"
+				+ "  1 - Search and Tick (speed over precision)\n"
+				+ "  2 - Full World Tick (precision over speed)");
+		this.method = builder.defineInRange("method", 1, 1, 2);
+		builder.comment("Waiting time before players are allowed to sleep again and skip time in seconds.");
+		this.waitTime = builder.defineInRange("waitTime", 250L, 5L, 600L);
+		builder.comment("A blacklist of what TileEntities that the loaded chunks method (method 1) will skip.");
 		this.blacklist = builder.define("blacklist", "minecraft:chest,minecraft:mob_spawner,quark:monster_box,mana-and-artifice:magelight,iceandfire:ghost_chest,minecraft:skull,mana-and-artifice:inscription_table_tile_entity,iceandfire:iaf_lectern,computercraft:speaker,computercraft:computer_advanced,computercraft:computer,mana-and-artifice:slipstream_generator,minecraft:trapped_chest");
 		this.spec = builder.build();
 	}
 
 	public ForgeConfigSpec getSpec() {
 		return this.spec;
+	}
+	
+	public int getSimulationMethod() {
+		return this.method.get();
+	}
+	
+	public boolean canSleep(long ticksElapsed) {
+		return ticksElapsed >= this.waitTime.get() * 20L;
 	}
 
 	public boolean tileEntityNotInBlacklist(TileEntity tileEntity) {
